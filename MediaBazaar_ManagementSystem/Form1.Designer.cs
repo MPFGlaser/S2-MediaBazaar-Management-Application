9ea5e6b6dcb6217403c2e57011d4c89f2bcf3b5c
Max Glaser <mpf.glaser@gmail.com>
2020-09-28 14:08:59 +0200

    Defined minimum size for main window

--- MediaBazaar_ManagementSystem/Form1.Designer.cs
+++ MediaBazaar_ManagementSystem/Form1.Designer.cs
@@ -451,6 +451,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "Form1";
             this.Text = "Media Bazaar Management System";
             this.tabControl1.ResumeLayout(false);
