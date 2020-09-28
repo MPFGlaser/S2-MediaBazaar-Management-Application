c67ff9f9ae8ed45dfdea4dbb3eca029adea452e7
Max Glaser <mpf.glaser@gmail.com>
2020-09-28 11:45:30 +0200

    Implemented a first version of the stock management UI

--- MediaBazaar_ManagementSystem/Form1.Designer.cs
+++ MediaBazaar_ManagementSystem/Form1.Designer.cs
@@ -31,9 +31,23 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStockAdd = new System.Windows.Forms.Button();
+            this.buttonStockChangeAmount = new System.Windows.Forms.Button();
+            this.dataGridViewStock = new System.Windows.Forms.DataGridView();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.productName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productBrand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productCategory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productQuantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productActive = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -63,19 +77,73 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.buttonStockAdd);
+            this.tabPage2.Controls.Add(this.buttonStockChangeAmount);
+            this.tabPage2.Controls.Add(this.dataGridViewStock);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Stock";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(638, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Toggle Active/Inactive";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonStockAdd
+            // 
+            this.buttonStockAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStockAdd.Location = new System.Drawing.Point(6, 371);
+            this.buttonStockAdd.Name = "buttonStockAdd";
+            this.buttonStockAdd.Size = new System.Drawing.Size(124, 23);
+            this.buttonStockAdd.TabIndex = 2;
+            this.buttonStockAdd.Text = "Add Product";
+            this.buttonStockAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonStockChangeAmount
+            // 
+            this.buttonStockChangeAmount.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonStockChangeAmount.Location = new System.Drawing.Point(324, 371);
+            this.buttonStockChangeAmount.Name = "buttonStockChangeAmount";
+            this.buttonStockChangeAmount.Size = new System.Drawing.Size(124, 23);
+            this.buttonStockChangeAmount.TabIndex = 1;
+            this.buttonStockChangeAmount.Text = "Change Amount";
+            this.buttonStockChangeAmount.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewStock
+            // 
+            this.dataGridViewStock.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStock.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productName,
+            this.productBrand,
+            this.productCode,
+            this.productCategory,
+            this.productQuantity,
+            this.productPrice,
+            this.productActive,
+            this.productDescription});
+            this.dataGridViewStock.Location = new System.Drawing.Point(6, 6);
+            this.dataGridViewStock.Name = "dataGridViewStock";
+            this.dataGridViewStock.Size = new System.Drawing.Size(756, 359);
+            this.dataGridViewStock.TabIndex = 0;
             // 
             // tabPage3
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(192, 74);
+            this.tabPage3.Size = new System.Drawing.Size(768, 400);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Statistics";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -84,10 +152,53 @@
             // 
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(192, 74);
+            this.tabPage4.Size = new System.Drawing.Size(768, 400);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Scheduling";
             this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // productName
+            // 
+            this.productName.HeaderText = "Product Name";
+            this.productName.Name = "productName";
+            // 
+            // productBrand
+            // 
+            this.productBrand.HeaderText = "Brand";
+            this.productBrand.Name = "productBrand";
+            // 
+            // productCode
+            // 
+            this.productCode.HeaderText = "Code";
+            this.productCode.Name = "productCode";
+            // 
+            // productCategory
+            // 
+            this.productCategory.HeaderText = "Category";
+            this.productCategory.Name = "productCategory";
+            // 
+            // productQuantity
+            // 
+            this.productQuantity.HeaderText = "Quantity";
+            this.productQuantity.Name = "productQuantity";
+            // 
+            // productPrice
+            // 
+            this.productPrice.HeaderText = "Price";
+            this.productPrice.Name = "productPrice";
+            // 
+            // productActive
+            // 
+            this.productActive.HeaderText = "Active";
+            this.productActive.Name = "productActive";
+            this.productActive.Width = 50;
+            // 
+            // productDescription
+            // 
+            this.productDescription.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.productDescription.HeaderText = "Description";
+            this.productDescription.MinimumWidth = 50;
+            this.productDescription.Name = "productDescription";
             // 
             // Form1
             // 
@@ -98,6 +209,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -109,6 +222,18 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView dataGridViewStock;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonStockAdd;
+        private System.Windows.Forms.Button buttonStockChangeAmount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productBrand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productCategory;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productQuantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productActive;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productDescription;
     }
 }
 
