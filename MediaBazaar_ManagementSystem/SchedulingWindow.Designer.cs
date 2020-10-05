72d35da385b15c0b430bc0e0c0d26e8c516f005a
Max Glaser <mpf.glaser@gmail.com>
2020-10-05 23:43:07 +0200

    Tweaked the design of Scheduling window

--- MediaBazaar_ManagementSystem/SchedulingWindow.Designer.cs
+++ MediaBazaar_ManagementSystem/SchedulingWindow.Designer.cs
@@ -43,14 +43,12 @@
             // 
             // textBoxCalendarDate
             // 
-            this.textBoxCalendarDate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxCalendarDate.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBoxCalendarDate.BackColor = System.Drawing.SystemColors.Control;
             this.textBoxCalendarDate.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxCalendarDate.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.textBoxCalendarDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.textBoxCalendarDate.Location = new System.Drawing.Point(41, 67);
+            this.textBoxCalendarDate.Location = new System.Drawing.Point(12, 35);
             this.textBoxCalendarDate.Name = "textBoxCalendarDate";
             this.textBoxCalendarDate.ReadOnly = true;
             this.textBoxCalendarDate.Size = new System.Drawing.Size(186, 23);
@@ -60,7 +58,7 @@
             // 
             // buttonPreviousDay
             // 
-            this.buttonPreviousDay.Location = new System.Drawing.Point(41, 96);
+            this.buttonPreviousDay.Location = new System.Drawing.Point(12, 70);
             this.buttonPreviousDay.Name = "buttonPreviousDay";
             this.buttonPreviousDay.Size = new System.Drawing.Size(33, 23);
             this.buttonPreviousDay.TabIndex = 4;
@@ -69,7 +67,7 @@
             // 
             // buttonNextDay
             // 
-            this.buttonNextDay.Location = new System.Drawing.Point(194, 96);
+            this.buttonNextDay.Location = new System.Drawing.Point(165, 70);
             this.buttonNextDay.Name = "buttonNextDay";
             this.buttonNextDay.Size = new System.Drawing.Size(33, 23);
             this.buttonNextDay.TabIndex = 5;
@@ -80,7 +78,7 @@
             // 
             this.comboBoxShiftTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxShiftTime.FormattingEnabled = true;
-            this.comboBoxShiftTime.Location = new System.Drawing.Point(249, 38);
+            this.comboBoxShiftTime.Location = new System.Drawing.Point(387, 12);
             this.comboBoxShiftTime.Name = "comboBoxShiftTime";
             this.comboBoxShiftTime.Size = new System.Drawing.Size(108, 21);
             this.comboBoxShiftTime.TabIndex = 6;
@@ -88,7 +86,7 @@
             // comboBoxSelectEmployees
             // 
             this.comboBoxSelectEmployees.FormattingEnabled = true;
-            this.comboBoxSelectEmployees.Location = new System.Drawing.Point(41, 127);
+            this.comboBoxSelectEmployees.Location = new System.Drawing.Point(12, 101);
             this.comboBoxSelectEmployees.Name = "comboBoxSelectEmployees";
             this.comboBoxSelectEmployees.Size = new System.Drawing.Size(186, 21);
             this.comboBoxSelectEmployees.Sorted = true;
@@ -97,7 +95,7 @@
             // 
             // buttonAddEmployeeToShift
             // 
-            this.buttonAddEmployeeToShift.Location = new System.Drawing.Point(249, 127);
+            this.buttonAddEmployeeToShift.Location = new System.Drawing.Point(90, 128);
             this.buttonAddEmployeeToShift.Name = "buttonAddEmployeeToShift";
             this.buttonAddEmployeeToShift.Size = new System.Drawing.Size(108, 23);
             this.buttonAddEmployeeToShift.TabIndex = 8;
@@ -107,7 +105,7 @@
             // 
             // buttonRemoveEmployeeFromShift
             // 
-            this.buttonRemoveEmployeeFromShift.Location = new System.Drawing.Point(249, 170);
+            this.buttonRemoveEmployeeFromShift.Location = new System.Drawing.Point(91, 157);
             this.buttonRemoveEmployeeFromShift.Name = "buttonRemoveEmployeeFromShift";
             this.buttonRemoveEmployeeFromShift.Size = new System.Drawing.Size(108, 23);
             this.buttonRemoveEmployeeFromShift.TabIndex = 9;
@@ -118,22 +116,20 @@
             // listBoxCurrentEmployees
             // 
             this.listBoxCurrentEmployees.FormattingEnabled = true;
-            this.listBoxCurrentEmployees.Location = new System.Drawing.Point(445, 127);
+            this.listBoxCurrentEmployees.Location = new System.Drawing.Point(205, 39);
             this.listBoxCurrentEmployees.Name = "listBoxCurrentEmployees";
-            this.listBoxCurrentEmployees.Size = new System.Drawing.Size(290, 251);
+            this.listBoxCurrentEmployees.Size = new System.Drawing.Size(290, 316);
             this.listBoxCurrentEmployees.Sorted = true;
             this.listBoxCurrentEmployees.TabIndex = 10;
             // 
             // textBoxWeekDay
             // 
-            this.textBoxWeekDay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxWeekDay.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBoxWeekDay.BackColor = System.Drawing.SystemColors.Control;
             this.textBoxWeekDay.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxWeekDay.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.textBoxWeekDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.textBoxWeekDay.Location = new System.Drawing.Point(41, 38);
+            this.textBoxWeekDay.Location = new System.Drawing.Point(12, 6);
             this.textBoxWeekDay.Name = "textBoxWeekDay";
             this.textBoxWeekDay.ReadOnly = true;
             this.textBoxWeekDay.Size = new System.Drawing.Size(186, 23);
@@ -143,7 +139,7 @@
             // 
             // buttonScheduleConfirm
             // 
-            this.buttonScheduleConfirm.Location = new System.Drawing.Point(660, 404);
+            this.buttonScheduleConfirm.Location = new System.Drawing.Point(420, 357);
             this.buttonScheduleConfirm.Name = "buttonScheduleConfirm";
             this.buttonScheduleConfirm.Size = new System.Drawing.Size(75, 23);
             this.buttonScheduleConfirm.TabIndex = 39;
@@ -154,7 +150,7 @@
             // buttonScheduleCancel
             // 
             this.buttonScheduleCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonScheduleCancel.Location = new System.Drawing.Point(249, 404);
+            this.buttonScheduleCancel.Location = new System.Drawing.Point(12, 357);
             this.buttonScheduleCancel.Name = "buttonScheduleCancel";
             this.buttonScheduleCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonScheduleCancel.TabIndex = 38;
@@ -166,7 +162,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(507, 392);
             this.Controls.Add(this.buttonScheduleConfirm);
             this.Controls.Add(this.buttonScheduleCancel);
             this.Controls.Add(this.textBoxWeekDay);
@@ -178,8 +174,10 @@
             this.Controls.Add(this.buttonNextDay);
             this.Controls.Add(this.buttonPreviousDay);
             this.Controls.Add(this.textBoxCalendarDate);
+            this.MaximumSize = new System.Drawing.Size(523, 431);
+            this.MinimumSize = new System.Drawing.Size(523, 431);
             this.Name = "SchedulingWindow";
-            this.Text = "SchedulingWindow";
+            this.Text = "Scheduling a shift";
             this.ResumeLayout(false);
             this.PerformLayout();
 
