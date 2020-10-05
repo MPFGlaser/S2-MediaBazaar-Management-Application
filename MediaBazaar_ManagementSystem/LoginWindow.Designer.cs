4c7b2de2076e09785e88c62bd221725e3af5dd78
Max Glaser <mpf.glaser@gmail.com>
2020-10-05 22:53:24 +0200

    Neatened up UI for login form

--- MediaBazaar_ManagementSystem/LoginWindow.Designer.cs
+++ MediaBazaar_ManagementSystem/LoginWindow.Designer.cs
@@ -29,47 +29,30 @@
         private void InitializeComponent()
         {
             this.textBoxLoginUsername = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBoxLoginPassword = new System.Windows.Forms.TextBox();
             this.buttonLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxLoginUsername
             // 
-            this.textBoxLoginUsername.Location = new System.Drawing.Point(70, 56);
+            this.textBoxLoginUsername.Location = new System.Drawing.Point(68, 77);
             this.textBoxLoginUsername.Name = "textBoxLoginUsername";
-            this.textBoxLoginUsername.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLoginUsername.Size = new System.Drawing.Size(196, 20);
             this.textBoxLoginUsername.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(70, 82);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 999;
-            this.textBox2.Text = "Password";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(70, 30);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 1000;
-            this.textBox3.Text = "Username";
             // 
             // textBoxLoginPassword
             // 
-            this.textBoxLoginPassword.Location = new System.Drawing.Point(70, 108);
+            this.textBoxLoginPassword.Location = new System.Drawing.Point(68, 119);
             this.textBoxLoginPassword.Name = "textBoxLoginPassword";
-            this.textBoxLoginPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLoginPassword.Size = new System.Drawing.Size(196, 20);
             this.textBoxLoginPassword.TabIndex = 1;
+            this.textBoxLoginPassword.UseSystemPasswordChar = true;
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(70, 152);
+            this.buttonLogin.Location = new System.Drawing.Point(117, 213);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(100, 23);
             this.buttonLogin.TabIndex = 1001;
@@ -77,16 +60,37 @@
             this.buttonLogin.UseVisualStyleBackColor = true;
             this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1002;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1003;
+            this.label2.Text = "Password";
+            // 
             // LoginWindow
             // 
+            this.AcceptButton = this.buttonLogin;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(232, 210);
+            this.ClientSize = new System.Drawing.Size(332, 248);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonLogin);
             this.Controls.Add(this.textBoxLoginPassword);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBoxLoginUsername);
+            this.MaximumSize = new System.Drawing.Size(348, 287);
+            this.MinimumSize = new System.Drawing.Size(348, 287);
             this.Name = "LoginWindow";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -97,9 +101,9 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBoxLoginUsername;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBoxLoginPassword;
         private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
