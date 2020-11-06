f4b3f4779c88aa382584fa7ea3267ff223d05369
Max Glaser <mpf.glaser@gmail.com>
2020-11-06 12:53:47 +0100

    Added methods to build preferredHours string and to pre-fill the checkboxes if an input has been given.

--- MediaBazaar_ManagementSystem/PreferredHours.Designer.cs
+++ MediaBazaar_ManagementSystem/PreferredHours.Designer.cs
@@ -379,6 +379,7 @@
             this.buttonConfirm.TabIndex = 5;
             this.buttonConfirm.Text = "Confirm";
             this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
             // 
             // PreferredHours
             // 
