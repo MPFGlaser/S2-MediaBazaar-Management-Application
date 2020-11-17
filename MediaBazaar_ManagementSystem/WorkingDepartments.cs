51c827dcc9b480fc0e457090298fcd56b5ecbb9f
Snobl,Jindra J.W <j.snobl@student.fontys.nl>
2020-11-17 15:59:20 +0100

    Continued work in issue #25. All departments are now loaded into the combobox and can be seleced and unselected

--- MediaBazaar_ManagementSystem/WorkingDepartments.cs
+++ MediaBazaar_ManagementSystem/WorkingDepartments.cs
@@ -22,28 +22,70 @@ namespace MediaBazaar_ManagementSystem
 
         public WorkingDepartments(string input, List<Department> allDepartments)
         {
+            List<int> selectedDepartments = getSelectedDepartments(input);
             InitializeComponent();
-            InitializeComboBox(allDepartments);
+            InitializeDepartments(allDepartments, selectedDepartments);
         }
         #region Logic
-        private void InitializeComboBox(List<Department> allDepartments)
+        private List<int> getSelectedDepartments(string input)
+        {
+            return new List<int>();
+        }
+
+        private void InitializeDepartments(List<Department> allDepartments, List<int> selectedDepartments)
         {
             foreach (Department d in allDepartments)
             {
-                comboBoxSelectDepartments.DisplayMember = "Text";
-                comboBoxSelectDepartments.ValueMember = "Department";
-                comboBoxSelectDepartments.Items.Add(new { Text = d.Name, Department = d });
+                if (selectedDepartments.Contains(d.Id))
+                {
+                    listBoxCurrentWorkingDepartments.DisplayMember = "Text";
+                    listBoxCurrentWorkingDepartments.ValueMember = "Department";
+                    listBoxCurrentWorkingDepartments.Items.Add(new { Text = d.Name, Department = d });
+                }
+                else
+                {
+                    comboBoxSelectDepartments.DisplayMember = "Text";
+                    comboBoxSelectDepartments.ValueMember = "Department";
+                    comboBoxSelectDepartments.Items.Add(new { Text = d.Name, Department = d });
+                }
             }
         }
 
         private void AddDepartmentToWorkable()
         {
+            // Checks if there's actually a department selected to be added
+            if (comboBoxSelectDepartments.SelectedIndex != -1)
+            {
+                // Ensures the right department object is used
+                Department selectedDepartment = (comboBoxSelectDepartments.SelectedItem as dynamic).Department;
 
+                // Adds the selected department to the listbox with currently selected departments.
+                listBoxCurrentWorkingDepartments.DisplayMember = "Text";
+                listBoxCurrentWorkingDepartments.ValueMember = "Department";
+                listBoxCurrentWorkingDepartments.Items.Add(new { Text = selectedDepartment.Name, Department = selectedDepartment });
+
+                // Removes the employee from the list of available employees and resets the selection index.
+                comboBoxSelectDepartments.Items.Remove(comboBoxSelectDepartments.SelectedItem);
+                comboBoxSelectDepartments.SelectedIndex = -1;
+            }
         }
 
         private void RemoveDepartmentFromWorkable()
         {
+            // Checks if there's actually an employee selected to be removed
+            if (listBoxCurrentWorkingDepartments.SelectedIndex != -1)
+            {
+                // Ensures the right department object is used
+                Department selectedDepartment = (listBoxCurrentWorkingDepartments.SelectedItem as dynamic).Department;
 
+                // Adds the selected employee to the combobox of available employees.
+                comboBoxSelectDepartments.DisplayMember = "Text";
+                comboBoxSelectDepartments.ValueMember = "Department";
+                comboBoxSelectDepartments.Items.Add(new { Text = selectedDepartment.Name, Department = selectedDepartment });
+
+                // Removes the employee from the listbox of currently scheduled employee.
+                listBoxCurrentWorkingDepartments.Items.Remove(listBoxCurrentWorkingDepartments.SelectedItem);
+            }
         }
 
         #endregion
