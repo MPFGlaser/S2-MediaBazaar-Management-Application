4baa413a286aa4ce4d16bb6aa6d1eeb1ce6178b9
Max Glaser <mpf.glaser@gmail.com>
2020-11-17 14:16:59 +0100

    Removed .classes from Employee.cs namespace to prevent scope errors

--- MediaBazaar_ManagementSystem/Classes/Employee.cs
+++ MediaBazaar_ManagementSystem/Classes/Employee.cs
@@ -9,7 +9,7 @@ using MediaBazaar_ManagementSystem.Models;
 // I don't know what this does, but it gives errors.
 //using MySql.Data.X.XDevAPI.Common;
 
-namespace MediaBazaar_ManagementSystem.classes
+namespace MediaBazaar_ManagementSystem
 {
     /// <summary>
     /// Class for an employee object. Contains all relevant information that's also found in the database's employee table.
