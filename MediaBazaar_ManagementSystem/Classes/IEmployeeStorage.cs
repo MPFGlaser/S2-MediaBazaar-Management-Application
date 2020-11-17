a3cabd2c1fecc2cf08c392a240e8b2a9439e18b3
Max Glaser <mpf.glaser@gmail.com>
2020-11-17 14:15:47 +0100

    Added IEmployeeStorage interface Ref #44

---
+++ MediaBazaar_ManagementSystem/Classes/IEmployeeStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaar_ManagementSystem.Classes
+{
+    public interface IEmployeeStorage
+    {
+        bool CreateEmployee(Employee employee);
+
+        bool UpdateEmployee(Employee employee);
+
+        Employee GetEmployee(int id);
+
+        List<Employee> GetEmployees(bool activeOnly);
+    }
+}
