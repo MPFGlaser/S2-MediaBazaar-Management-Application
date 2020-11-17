c1b21a8f6847c82084d8bbb904be51b058a11707
Max Glaser <mpf.glaser@gmail.com>
2020-11-17 16:00:47 +0100

    Created ILoginStorage interface and LoginMySQL class Ref #44

---
+++ MediaBazaar_ManagementSystem/Classes/Login/LoginMySQL.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaBazaar_ManagementSystem
+{
+    class LoginMySQL : ILoginStorage
+    {
+        MySqlConnection connection;
+        string connectionString;
+
+        public LoginMySQL()
+        {
+            connectionString = ConnectionString.Read();
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public int Check(string username, string password)
+        {
+            int output = -1;
+
+            String query = "SELECT id FROM employees WHERE username = @username AND password = @password";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@username", username);
+            command.Parameters.AddWithValue("@password", Encrypt.Run(password));
+
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    output = Convert.ToInt32(reader[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong.\n" + ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return output;
+        }
+    }
+}
