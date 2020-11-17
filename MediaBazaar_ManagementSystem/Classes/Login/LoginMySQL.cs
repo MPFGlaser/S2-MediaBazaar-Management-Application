d9bbd88dd58fbe9c2bb5f86516c3f17ce1e752d5
Max Glaser <mpf.glaser@gmail.com>
2020-11-17 21:21:53 +0100

    Added comments to LoginMySQL class

--- MediaBazaar_ManagementSystem/Classes/Login/LoginMySQL.cs
+++ MediaBazaar_ManagementSystem/Classes/Login/LoginMySQL.cs
@@ -15,6 +15,12 @@ namespace MediaBazaar_ManagementSystem
             connection = new MySqlConnection(connectionString);
         }
 
+        /// <summary>
+        /// Checks if the given username/password combination is found in the database.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>The userId if one is found with the given credentials.</returns>
         public int Check(string username, string password)
         {
             int output = -1;
