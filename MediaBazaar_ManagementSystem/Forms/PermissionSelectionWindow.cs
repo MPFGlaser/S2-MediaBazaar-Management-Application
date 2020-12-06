a584185a0bcf197e20185fb2c82a32e35efb9f71
Max Glaser <mpf.glaser@gmail.com>
2020-12-06 15:39:52 +0100

    Created UI for function permission selection window. Ref

---
+++ MediaBazaar_ManagementSystem/Forms/PermissionSelectionWindow.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Forms;
+
+namespace MediaBazaar_ManagementSystem
+{
+    public partial class PermissionSelectionWindow : Form
+    {
+        public PermissionSelectionWindow()
+        {
+            InitializeComponent();
+            SetUniformHeight();
+        }
+
+        // if a manager, function management should all be ticked & greyed out to prevent locking oneself out!
+
+        private void SetUniformHeight()
+        {
+            int maxHeightFound = 0;          
+
+            foreach (Control c in this.flowLayoutPanel.Controls)
+            {
+                if (c is GroupBox)
+                {
+                    if (c.Height > maxHeightFound)
+                    {
+                        maxHeightFound = c.Height;
+                    }
+                }
+            }
+
+            if (maxHeightFound != 0)
+            {
+                foreach (Control c in this.flowLayoutPanel.Controls)
+                {
+                    if (c is GroupBox)
+                    {
+                        ((GroupBox)c).AutoSize = false;
+                        c.Height = maxHeightFound;
+                    }
+                }
+            }
+        }
+
+        private void flowLayoutPanel_Paint(object sender, PaintEventArgs e)
+        {
+            if(this.flowLayoutPanel.Width <= 478)
+            {
+                foreach (Control c in this.flowLayoutPanel.Controls)
+                {
+                    if (c is GroupBox)
+                    {
+                        ((GroupBox)c).AutoSize = true;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Control c in this.flowLayoutPanel.Controls)
+                {
+                    if (c is GroupBox)
+                    {
+                        ((GroupBox)c).AutoSize = false;
+                    }
+                }
+                SetUniformHeight();
+            }
+        }
+    }
+}
