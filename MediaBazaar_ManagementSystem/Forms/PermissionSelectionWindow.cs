04c19e08381dfe9cb99edf7f43b866d0edc3c1f5
Max Glaser <mpf.glaser@gmail.com>
2020-12-07 09:56:03 +0100

    Added comments to PermissionSelectionWindow.cs Ref: #68

--- MediaBazaar_ManagementSystem/Forms/PermissionSelectionWindow.cs
+++ MediaBazaar_ManagementSystem/Forms/PermissionSelectionWindow.cs
@@ -12,6 +12,10 @@ namespace MediaBazaar_ManagementSystem
 
         // if a manager, function management should all be ticked & greyed out to prevent locking oneself out!
 
+        /// <summary>
+        /// Sets the height of the groupboxes uniform to be the same as the greatest height found amongst all of them.
+        /// <para>This is so it looks nice when tiled in more than 1 column.</para>
+        /// </summary>
         private void SetUniformHeight()
         {
             int maxHeightFound = 0;          
@@ -40,6 +44,13 @@ namespace MediaBazaar_ManagementSystem
             }
         }
 
+        /// <summary>
+        /// Every time the size of the flowLayoutPanel is updated, it checks if the width is smaller than necessary for it to tile two controls next to each other.
+        /// <para>When that's the case, the extra blank space in some controls is removed.</para>
+        /// <para>When it's wide enough again to also tile horizontally, the blank space is added again.</para>
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="PaintEventArgs"/> instance containing the event data.</param>
         private void flowLayoutPanel_Paint(object sender, PaintEventArgs e)
         {
             if(this.flowLayoutPanel.Width <= 478)
