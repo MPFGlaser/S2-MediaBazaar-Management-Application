b5fd6ebddad81d15a3878669c9f7260448338ccb
Max Glaser <mpf.glaser@gmail.com>
2020-12-07 17:08:47 +0100

    Created IFunctionStorage interface

---
+++ MediaBazaar_ManagementSystem/Classes/Function/IFunctionStorage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MediaBazaar_ManagementSystem
+{
+    public interface IFunctionStorage
+    {
+        bool Create();
+
+        bool Update();
+
+        Dictionary<int, string> GetFunctions();
+
+        List<string> GetPermissions(int function);
+    }
+}
