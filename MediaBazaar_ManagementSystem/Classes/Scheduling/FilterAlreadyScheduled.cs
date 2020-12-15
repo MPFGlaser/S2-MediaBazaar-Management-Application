60137e06a360986373002e72769e67545e5895e4
Max Glaser <mpf.glaser@gmail.com>
2020-12-15 20:17:21 +0100

    Laid some groundwork for the filtering needed for automated scheduling. Ref: #40 #85

---
+++ MediaBazaar_ManagementSystem/Classes/Scheduling/FilterAlreadyScheduled.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediaBazaar_ManagementSystem
+{
+    public class FilterAlreadyScheduled : IFilter
+    {
+        public List<Employee> Filter(int shiftId, int departmentId, List<Employee> employees)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MediaBazaar_ManagementSystem/Classes/Scheduling/IFilter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MediaBazaar_ManagementSystem
+{
+    public interface IFilter
+    {
+        List<Employee> Filter(int shiftId, int departmentId, List<Employee> employees);
+    }
+}
