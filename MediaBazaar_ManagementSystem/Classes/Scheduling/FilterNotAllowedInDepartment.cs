819ca9f8ca5ea065a9908078323eb650bcaf9fbd
Max Glaser <mpf.glaser@gmail.com>
2021-01-04 17:24:07 +0100

    Added IFilter FilterNotAllowedInDepartment implementation Ref: #85

---
+++ MediaBazaar_ManagementSystem/Classes/Scheduling/FilterNotAllowedInDepartment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaar_ManagementSystem
+{
+    public class FilterNotAllowedInDepartment : IFilter
+    {
+        public List<Employee> Filter(Shift shift, int departmentId, List<Shift> weekShifts, List<WorkingEmployee> workingEmployees, List<Employee> employees)
+        {
+            List<Employee> output = new List<Employee>();
+
+            foreach(Employee employee in employees)
+            {
+                string[] workingDepartments = employee.WorkingDepartments.Split(',');
+
+                if (workingDepartments.Contains(departmentId.ToString()))
+                {
+                    output.Add(employee);
+                }
+            }
+
+            return output;
+        }
+    }
+}
