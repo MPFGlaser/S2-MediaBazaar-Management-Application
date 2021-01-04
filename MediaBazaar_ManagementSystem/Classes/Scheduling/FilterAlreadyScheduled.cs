1d0f35cb5eacef1092c7bbc1eec119e4baa53118
Max Glaser <mpf.glaser@gmail.com>
2021-01-04 15:44:48 +0100

    Added IFilter implementation FilterAlreadyScheduled Ref: #85

--- MediaBazaar_ManagementSystem/Classes/Scheduling/FilterAlreadyScheduled.cs
+++ MediaBazaar_ManagementSystem/Classes/Scheduling/FilterAlreadyScheduled.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MediaBazaar_ManagementSystem
 {
@@ -7,7 +8,22 @@ namespace MediaBazaar_ManagementSystem
     {
         public List<Employee> Filter(Shift shift, List<Shift> weekShifts, List<WorkingEmployee> workingEmployees, List<Employee> employees)
         {
-            throw new NotImplementedException();
+            List<Employee> output = new List<Employee>();
+
+            // Removes all irrelevant entries (i.e. anything not related to this shift) from workingEmployees
+            workingEmployees.RemoveAll(workingEmployee => workingEmployee.ShiftId != shift.Id);
+
+            // Loops through each employee
+            foreach (Employee employee in employees)
+            {
+                // If an employeeId/shiftId combination doesn't exist, add the employee to output.
+                if(!workingEmployees.Any(WorkingEmployee => WorkingEmployee.EmployeeId == employee.Id))
+                {
+                    output.Add(employee);
+                }
+            }
+
+            return output;
         }
     }
 }
