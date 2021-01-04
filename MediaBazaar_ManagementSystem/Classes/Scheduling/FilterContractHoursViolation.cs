33acbab96467ed1f4cc0942ac2295712852c86fe
Max Glaser <mpf.glaser@gmail.com>
2021-01-04 17:15:12 +0100

    Added IFilter FilterContractHoursViolation implementation Ref: #85

--- MediaBazaar_ManagementSystem/Classes/Scheduling/FilterContractHoursViolation.cs
+++ MediaBazaar_ManagementSystem/Classes/Scheduling/FilterContractHoursViolation.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MediaBazaar_ManagementSystem
 {
     public class FilterContractHoursViolation : IFilter
     {
-        public List<Employee> Filter(Shift shift, List<Shift> weekShifts, List<WorkingEmployee> workingEmployees, List<Employee> employees)
+        public List<Employee> Filter(Shift shift, int departmentId, List<Shift> weekShifts, List<WorkingEmployee> workingEmployees, List<Employee> employees)
         {
-            throw new NotImplementedException();
+            List<Employee> output = new List<Employee>();
+
+            foreach(Employee employee in employees)
+            {
+                float hoursWorkedThisWeek = 0;
+
+                foreach(Shift s in weekShifts)
+                {
+                    if(workingEmployees.Any(workingEmployee =>
+                    workingEmployee.ShiftId == s.Id &&
+                    workingEmployee.EmployeeId == employee.Id))
+                    {
+                        hoursWorkedThisWeek += Globals.shiftDuration;
+                    }
+                }
+
+                if(employee.ContractHours >= (hoursWorkedThisWeek + Globals.shiftDuration))
+                {
+                    output.Add(employee);
+                }
+            }
+
+            return output;
         }
     }
 }
