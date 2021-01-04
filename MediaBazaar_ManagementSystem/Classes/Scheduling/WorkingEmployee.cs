d3a1610cd752002b88509f4ad460917b09fb9531
Snobl,Jindra J.W <j.snobl@student.fontys.nl>
2021-01-04 11:00:38 +0100

    Added class WorkingEmployee

---
+++ MediaBazaar_ManagementSystem/Classes/Scheduling/WorkingEmployee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaar_ManagementSystem.Classes.Scheduling
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class WorkingEmployee
+    {
+        private int shiftId, employeeId, departmentId;
+
+        public WorkingEmployee(int shiftId, int employeeId, int departmentId)
+        {
+            this.shiftId = shiftId;
+            this.employeeId = employeeId;
+            this.departmentId = departmentId;
+        }
+
+        #region Properties
+        public int ShiftId
+        {
+            get { return shiftId; }
+        }
+
+        public int EmployeeId
+        {
+            get { return employeeId; }
+        }
+
+        public int DepartmentId
+        {
+            get { return departmentId; }
+        }
+
+        #endregion
+    }
+}
